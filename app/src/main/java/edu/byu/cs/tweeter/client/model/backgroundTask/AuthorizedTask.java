fcef8e20a5c4606232675e6f6431c64405d5d5e1
Tom Bates <tombates6@gmail.com>
2022-02-02 10:20:27 -0700

    Create top-level Task abstraction

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/backgroundTask/AuthorizedTask.java
@@ -0,0 +1,18 @@
+package edu.byu.cs.tweeter.client.model.backgroundTask;
+
+import android.os.Handler;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+
+public abstract class AuthorizedTask extends BackgroundTask {
+
+    /**
+     * Auth token for logged-in user.
+     */
+    protected final AuthToken authToken;
+
+    public AuthorizedTask(Handler messageHandler, AuthToken authToken) {
+        super(messageHandler);
+        this.authToken = authToken;
+    }
+}
